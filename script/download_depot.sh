f0ece22a4d94f4bdb3a04fe026f86b37db4bdcce
Auj625197595 <32589249+Auj625197595@users.noreply.github.com>
2024-06-29 22:17:30 +0800

    Update download_depot.sh

--- script/download_depot.sh
+++ script/download_depot.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 git clone --depth=1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
+cd depot_tools
+git checkout c601e364af -b b57
+cd ../
 export DEPOT_TOOLS_DIR=$PWD/depot_tools
 export PATH=$DEPOT_TOOLS_DIR:$DEPOT_TOOLS_DIR/python2_bin:$DEPOT_TOOLS_DIR/python_bin:$PATH
 echo "$DEPOT_TOOLS_DIR" >> $GITHUB_PATH
