75abed199eac1c866c9824bbcbe8e2eb7336e0d2
Auj625197595 <32589249+Auj625197595@users.noreply.github.com>
2024-06-29 23:38:18 +0800

    Update first_run.sh
    sed -i "s#is_java_debug = false#is_java_debug = true#" out/arm64/args.gn

--- script/first_run.sh
+++ script/first_run.sh
@@ -61,6 +61,7 @@ cd src
 mkdir -p out/arm64/
 cat .build/android_arm/args.gn | sed "s#target_cpu = \"arm\"#target_cpu = \"arm64\"#" | sed "s#android_default_version_name = \"Git\"#android_default_version_name = \"Git$(date '+%y%m%d')\"#" > out/arm64/args.gn
 sed -i "s#android_default_version_code = \"1\"#android_default_version_code = \"$(date '+%y%m%d')\"#" out/arm64/args.gn
+sed -i "s#is_java_debug = false#is_java_debug = true#" out/arm64/args.gn
 cat out/arm64/args.gn
 gn gen out/arm64/
 
