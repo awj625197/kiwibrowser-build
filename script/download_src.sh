e9a05fa70e8f2d87c81ff29b7555e036071c13d8
Auj625197595 <32589249+Auj625197595@users.noreply.github.com>
2024-06-29 22:43:15 +0800

    Update download_src.sh
    export DEPOT_TOOLS_UPDATE=0

--- script/download_src.sh
+++ script/download_src.sh
@@ -9,4 +9,5 @@ sudo bash install-build-deps.sh --no-chromeos-fonts
 build/linux/sysroot_scripts/install-sysroot.py --arch=i386
 build/linux/sysroot_scripts/install-sysroot.py --arch=amd64
 keytool -genkey -v -keystore keystore.jks -alias dev -keyalg RSA -keysize 2048 -validity 10000 -storepass public_password -keypass public_password -dname "cn=Kiwi Browser, ou=Actions, o=Kiwi Browser, c=GitHub"
+export DEPOT_TOOLS_UPDATE=0
 gclient runhooks
