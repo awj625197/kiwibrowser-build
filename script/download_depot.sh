a71c1d495a96ab503bd9c1cbbc9d4de3df400c65
awj625197 <1440382174@qq.com>
2024-06-30 20:36:44 +0800

    Update download_depot.sh

--- script/download_depot.sh
+++ script/download_depot.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 cd depot_tools
-git checkout c601e364af -b b57
+git checkout b5cec8c867 -b b57
 cd ../
 export DEPOT_TOOLS_DIR=$PWD/depot_tools
 export PATH=$DEPOT_TOOLS_DIR:$DEPOT_TOOLS_DIR/python2_bin:$DEPOT_TOOLS_DIR/python_bin:$PATH
