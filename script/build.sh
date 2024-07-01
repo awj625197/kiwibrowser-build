e94d0331f7a6d8963978b24589f30af153268317
awj625197 <1440382174@qq.com>
2024-07-01 20:05:00 +0800

    Update build.sh

--- script/build.sh
+++ script/build.sh
@@ -30,6 +30,7 @@ cat .build/android_arm/args.gn | sed "s#target_cpu = \"arm\"#target_cpu = \"arm6
 sed -i "s#android_default_version_code = \"1\"#android_default_version_code = \"$(date '+%y%m%d')\"#" out/arm64/args.gn
 sed -i "s#is_java_debug = false#is_java_debug = true#" out/arm64/args.gn
 sed -i "s#is_debug = false#is_debug = true#" out/arm64/args.gn
+sed -i "s#is_official_build = true#is_official_build = false#" out/arm64/args.gn
 echo "\nfatal_linker_warnings = false" >> out/arm64/args.gn
 
 cat out/arm64/args.gn
