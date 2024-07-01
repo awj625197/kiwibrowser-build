6e32d5b920786501299d15cef3910e110ef0cd8a
awj625197 <1440382174@qq.com>
2024-07-01 19:48:19 +0800

    Update first_run.sh

--- script/first_run.sh
+++ script/first_run.sh
@@ -62,6 +62,7 @@ mkdir -p out/arm64/
 cat .build/android_arm/args.gn | sed "s#target_cpu = \"arm\"#target_cpu = \"arm64\"#" | sed "s#android_default_version_name = \"Git\"#android_default_version_name = \"Git$(date '+%y%m%d')\"#" > out/arm64/args.gn
 sed -i "s#android_default_version_code = \"1\"#android_default_version_code = \"$(date '+%y%m%d')\"#" out/arm64/args.gn
 sed -i "s#is_java_debug = false#is_java_debug = true#" out/arm64/args.gn
+sed -i "s#is_official_build = true#is_official_build = false#" out/arm64/args.gn
 echo "\nfatal_linker_warnings = false" >> out/arm64/args.gn
 cat out/arm64/args.gn
 gn gen out/arm64/
